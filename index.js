e00c38f778a3183bf23d28c43a119600ddb92469
rakuraman2005 <rakuraman2005@gmail.com>
2024-07-26 09:56:13 +0530

    animations added

--- index.js
+++ index.js
@@ -3,4 +3,19 @@ let burger = document.querySelector("#burger");
 let Links = document.querySelector("#nav-li");
 burger.addEventListener('click',()=> {
     Links.classList.toggle("nav-show");
-})
+});
+
+ScrollReveal({ 
+    reset: true,
+    distance:'80px',
+    duration:1300,
+    delay: 10
+
+ });
+
+ ScrollReveal().reveal('#role', { delay: 500 , origin:'left' });
+ ScrollReveal().reveal('h1', { delay: 100 , origin:'left' });
+ ScrollReveal().reveal('p', { delay: 0 , origin:'bottom' });
+ ScrollReveal().reveal('#main', { delay: 0 , origin:'right' });
+ 
+ ScrollReveal().reveal('.bt', { delay: 700 , origin:'bottom' });
